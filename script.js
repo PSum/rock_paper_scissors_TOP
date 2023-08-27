7ef669b0a578376a1e6494df75ecd0d06be72656
Philipp <50810270+PSum@users.noreply.github.com>
2023-08-27 16:44:33 +0200

    basic ui and basic logic are finished
    I need to implement something, so the skript stops when someones score is 5

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-function askPlayer() {
-    let playerValue = prompt("Make your move! \n 1 : Rock \n 2 : Paper \n 3: Scissors");
-    return playerValue;
-}
+let scorePlayer = 0;
+let scoreComputer = 0;
+
+const btnRock = document.querySelector("#btn-Rock");
+const btnPaper = document.querySelector("#btn-Paper");
+const btnScissors = document.querySelector("#btn-Scissors");
 
 function randomInt () {
     let max = 5;
@@ -10,57 +12,65 @@ function randomInt () {
     return computerValue;
 }
 
-function playGame(turns) {
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    for (i=0; i < turns; i++) {
-        let answerPlayer = parseInt(askPlayer());
-        console.log(answerPlayer)
-        let answerComputer = randomInt();
-        if (answerPlayer == 1) {
-            console.log("You chose Rock")
-        } else if (answerPlayer == 2) {
-            console.log("You chose Paper")
-        } else if (answerPlayer == 3) {
-            console.log("You chose Paper")
-        }
-        switch (answerComputer) {
-            case 1:
-            case 4:
-                console.log("Computer chose Rock");
-                break;
-            case 2:
-            case 5:
-                console.log("Computer chose Paper");
-                break;
-            case 0:
-            case 3:
-                console.log("Computer chose Scissors");
-                break;
-        }
-
-            switch (answerPlayer) {
-                case (answerComputer + 1):
-                case (answerComputer + 4):
-                case (answerComputer - 2):
-                    console.log("You win this round!");
-                    scorePlayer += 1;
-                    break;
-                case (answerComputer - 1):
-                case (answerComputer + 2):
-                case (answerComputer - 4):
-                    console.log("You loose this round");
-                    scoreComputer += 1;
-                    break;
-                default:
-                    console.log("Nobody wins");
-                    break;
-            }
-        console.log(`The score is:\n You: ${scorePlayer} \n Computer: ${scoreComputer}`)
-
+function playGame(playerInput) {
+    let answerPlayer = parseInt(playerInput);
+    console.log(answerPlayer)
+    let answerComputer = randomInt();
+    if (answerPlayer == 1) {
+        console.log("You chose Rock")
+    } else if (answerPlayer == 2) {
+        console.log("You chose Paper")
+    } else if (answerPlayer == 3) {
+        console.log("You chose Scissors")
     }
+    switch (answerComputer) {
+        case 1:
+        case 4:
+            console.log("Computer chose Rock");
+            break;
+        case 2:
+        case 5:
+            console.log("Computer chose Paper");
+            break;
+        case 0:
+        case 3:
+            console.log("Computer chose Scissors");
+            break;}
+
+        switch (answerPlayer) {
+            case (answerComputer + 1):
+            case (answerComputer + 4):
+            case (answerComputer - 2):
+                console.log("You win this round!");
+                scorePlayer += 1;
+                break;
+            case (answerComputer - 1):
+            case (answerComputer + 2):
+            case (answerComputer - 4):
+                console.log("You loose this round");
+                scoreComputer += 1;
+                break;
+            default:
+                console.log("Nobody wins");
+                break;
+            }
+    document.querySelector(".spieler").innerHTML = scorePlayer;
+    document.querySelector(".computer").innerHTML = scoreComputer;
 }
 
-let turns = prompt("Wie viele Runden?");
-playGame(turns);
+
+
+
+
+btnRock.addEventListener("click", function () {
+    playGame(1);
+})
+btnPaper.addEventListener("click", function () {
+    playGame(2);
+})
+btnScissors.addEventListener("click", function () {
+    playGame(3);
+})
+
+
 
